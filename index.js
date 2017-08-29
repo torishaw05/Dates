c25e9220503ad71bb92cd0840fa0d3d56495d387
Tori Shaw <torishaw@toris-air.lan>
2017-08-29 10:06:58 -0400

    updated time & seconds

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const moment = require("moment");
 const chalk = require("chalk");
 
+
     console.log("It is" +" "+ chalk.blue(moment().format('LLLL')) + " .");
     console.log("It is" + " "+ chalk.green(moment().format('DDD'))+" " + "day of the year.");
 
-
+console.log("It is" + chalk.magenta(moment().hours() * 60 * 60 + moment().minutes() * 60 + moment().seconds()) + 'seconds of the day.');
 console.log("It is" + " " + chalk.yellow( moment([2017, 8, 14]).isDST()) + " " + " August 14, 2017 is during Daylight Savings Time.");
 console.log("It is" + " " + chalk.red( moment([2001]).isLeapYear()) + " " + " August 14, 2017 is not a leap year.");
